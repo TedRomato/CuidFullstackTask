0ababddcf615b12d183b1876ca6bf9184f266bee
Ondřej Vainer <ondrej.vainer@unicorn.com>
2023-09-22 18:07:40 +0200

    BE implementation

--- fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
@@ -1,14 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { ExchangeRate } from '@cuid/entities';
+import axios from 'axios';
+import Constants from '../../config/constants';
+
+async function fetchRatesData(url: string) {
+    let ratesData: string;
+    try {
+        ratesData = (await axios.get(url)).data;
+    } catch (error) {
+        console.error(new Error(`Couldn't reach ${URL} to fetch rates data.`));
+    }
+
+    return ratesData;
+}
+
+function parseRatesData(ratesData: string) {
+    const regex = /(?<country>.+)[|](?<currncy>.+)[|](?<amount>.+)[|](?<code>.+)[|](?<rate>.+)/gm;
+    const rates = [];
+
+    [...ratesData.matchAll(regex)].forEach((match) => {
+        const parsedRate = { ...match.groups };
+        const rate = {
+            ...parsedRate,
+            amount: parseInt(parsedRate.amount, 10),
+            rate: parseFloat(parsedRate.rate),
+        };
+        rates.push(rate);
+    });
+
+    // We have to remove the first result as it doesn't contain rates data, but information about order of rate attributes
+    rates.shift();
+    return rates;
+}
 
 @Injectable()
 export class ExchangeRateService {
-    public getExchangeRates = async () => {
-        const rates: ExchangeRate[] = [];
+    async getExchangeRates() {
+        let rates: ExchangeRate[] = [];
 
-        // TODO: Implement the fetching and parsing of the exchange rates.
-        // Use this method in the resolver.
+        const ratesData: string = await fetchRatesData(Constants.RATES_URL);
+        rates = parseRatesData(ratesData);
 
         return rates;
-    };
+    }
 }
--- fullstack/task/packages/web/src/App.tsx
+++ fullstack/task/packages/web/src/App.tsx
@@ -1,9 +1,34 @@
+import { ApolloConsumer, gql } from '@apollo/client';
+
 function App() {
     // TODO: Add the FE for exchange rates here.
 
     return (
         <div>
-            <p>Add the exchange rates here</p>
+            <ApolloConsumer>
+                {(client) => (
+                    <>
+                        <p>Add the exchange rates here</p>
+
+                        {(() => {
+                            client
+                                .query({
+                                    query: gql`
+                                        {
+                                            exchangeRates {
+                                                country
+                                                currncy
+                                            }
+                                        }
+                                    `,
+                                })
+                                .then((result) => console.log(result));
+
+                            return null;
+                        })()}
+                    </>
+                )}
+            </ApolloConsumer>
         </div>
     );
 }
